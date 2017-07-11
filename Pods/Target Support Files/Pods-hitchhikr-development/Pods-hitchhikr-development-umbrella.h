416bc1aca1c07a3de4f3100e938db3132d87534e
Brian Lane <brian@gourmetgaming.com>
2017-07-11 10:03:49 -0400

    Created first storyboard

---
+++ Pods/Target Support Files/Pods-hitchhikr-development/Pods-hitchhikr-development-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_hitchhikr_developmentVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_hitchhikr_developmentVersionString[];
+
